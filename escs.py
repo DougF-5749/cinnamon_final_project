495e383079bb222246e89eb0d66d8aaa3b1e731e
Benedict Valuks <benedictvaluks@MASTUDE44FAQ6L4.mynet>
2025-12-11 13:57:59 +0000

    Tripled average escs to get better visuals on dashboard

--- escs.py
+++ escs.py
@@ -4,7 +4,7 @@ def esc_scores(conn):
         """
         SELECT
             country,
-            AVG(econ_soc_cul_status)
+            AVG(econ_soc_cul_status) * 3
         FROM analytical_responses
         GROUP BY country
         """
